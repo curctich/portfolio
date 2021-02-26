402d1fe3fd9595a1a800dd16d6c7d802e62bd9d8
saori <sieeya.bp@gmail.com>
2021-02-26 23:56:30 +0900

    ヘッダーのスタイルを作成

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,35 @@
 <template>
   <header>
-    <nav>
-      <ul>
-        <li>HOME</li>
-        <li>WORKS</li>
-      </ul>
-    </nav>
+    <div class="main-area header__wrapper">
+      <nav>
+        <ul class="header__nav">
+          <li class="header__nav_item">
+            <nuxt-link to="/">HOME</nuxt-link>
+          </li>
+          <li class="header__nav_item">
+            <nuxt-link to="/">WORKS</nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
-<style>
-
+<style lang="scss" scoped>
+.header__wrapper {
+  display: flex;
+  justify-content: flex-end;
+}
+.header__nav {
+  display: flex;
+}
+.header__nav_item {
+  padding: 15px;
+  font-size: 1.6rem;
+  font-weight: bold;
+  transition: all 0.5s;
+  &:hover {
+    opacity: 0.5;
+  }
+}
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -12,7 +12,7 @@
 // }
 </script>
 
-<style>
+<style lang="scss">
 /*html {*/
 /*  font-family:*/
 /*    'Source Sans Pro',*/
@@ -31,40 +31,25 @@
 /*  -webkit-font-smoothing: antialiased;*/
 /*  box-sizing: border-box;*/
 /*}*/
+html {
+  font-size: 62.5%;
+  color: #212121;
+}
 
-/**,*/
-/**::before,*/
-/**::after {*/
-/*  box-sizing: border-box;*/
-/*  margin: 0;*/
-/*}*/
+a {
+  text-decoration: none;
+  color: #212121;
+}
 
-/*.button--green {*/
-/*  display: inline-block;*/
-/*  border-radius: 4px;*/
-/*  border: 1px solid #3b8070;*/
-/*  color: #3b8070;*/
-/*  text-decoration: none;*/
-/*  padding: 10px 30px;*/
-/*}*/
+ul {
+  margin: 0;
+  padding-left: 0;
+  list-style: none;
+}
 
-/*.button--green:hover {*/
-/*  color: #fff;*/
-/*  background-color: #3b8070;*/
-/*}*/
-
-/*.button--grey {*/
-/*  display: inline-block;*/
-/*  border-radius: 4px;*/
-/*  border: 1px solid #35495e;*/
-/*  color: #35495e;*/
-/*  text-decoration: none;*/
-/*  padding: 10px 30px;*/
-/*  margin-left: 15px;*/
-/*}*/
-
-/*.button--grey:hover {*/
-/*  color: #fff;*/
-/*  background-color: #35495e;*/
-/*}*/
+.main-area {
+  width: 100%;
+  max-width: 960px;
+  margin: 0 auto;
+}
 </style>
